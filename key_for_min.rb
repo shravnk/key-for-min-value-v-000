55caaa9412fb97ec139bdb7a12fc24835dab2eb6
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-01 05:00:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if value < lowest_value
       lowest_value = value
-      lowest_key = name_hash
+      lowest_key = name
     end
   end
   lowest_key
